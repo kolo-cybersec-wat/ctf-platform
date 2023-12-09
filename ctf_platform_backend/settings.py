3c2ba050dd404a1e6ea92e7ee63572a13fd8400a
Michał Oręziak <micorix.dev@gmail.com>
2023-12-09 03:33:42 +0100

    Parametrized db access

--- ctf_platform_backend/settings.py
+++ ctf_platform_backend/settings.py
@@ -106,11 +106,11 @@ WSGI_APPLICATION = "ctf_platform_backend.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",
-        "PORT": "5432",
+        "NAME": os.environ.get("DB_NAME", "postgres"),
+        "USER": os.environ.get("DB_USER", "postgres"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
